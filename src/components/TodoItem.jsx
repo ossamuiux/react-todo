336005b0d345813298dbaf11edd021e66a071784
ossamuiux <dalhoya@naver.com>
2023-12-21 18:09:16 +0900

    feat: 리액트 투두 완료 #2

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -12,6 +12,16 @@ export default function TodoItem({ todo, todos, setTodos }) {
     );
   }
 
+  // todos배열요소 모두(t)에서 todo.id가 다른 요소로
+  // 새로운 배열 생성하여 setTodos로 todos배열 업데이트
+  function handleRemove() {
+    setTodos(
+      todos.filter((t) => {
+        return t.id !== todo.id;
+      })
+    );
+  }
+
   return (
     <li className={styles.todo_item}>
       <input id={`check${todo.id}`} type="checkbox" />
@@ -19,7 +29,9 @@ export default function TodoItem({ todo, todos, setTodos }) {
       <label htmlFor={`check${todo.id}`} onClick={handleDone} className={todo.done === true ? styles.done : ''}>
         {todo.text}
       </label>
-      <button type="button">remove</button>
+      <button type="button" onClick={handleRemove}>
+        remove
+      </button>
     </li>
   );
 }
